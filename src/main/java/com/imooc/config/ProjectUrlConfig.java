b8a400ca4afc17abebddec630bf3ec48786356e3
洪陈金 <hongcj@guahao.com>
2017-08-09 16:49:01 +0800

    [chg]增加微信授权的第三方SDK的使用

---
+++ src/main/java/com/imooc/config/ProjectUrlConfig.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2001-2017 GuaHao.com Corporation Limited. All rights reserved. 
+ * This software is the confidential and proprietary information of GuaHao Company. 
+ * ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only 
+ * in accordance with the terms of the license agreement you entered into with GuaHao.com.
+ */
+package com.imooc.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+/**
+ *
+ * @author hongcj
+ * @version V1.0
+ * @since 2017-08-09 15:02
+ */
+@Component
+@Data
+@ConfigurationProperties(prefix = "projectUrl")
+public class ProjectUrlConfig {
+    private String wechatMpAuthorize;
+}
--- src/main/java/com/imooc/controller/WeixinController.java
+++ src/main/java/com/imooc/controller/WeixinController.java
@@ -11,11 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- *
+ * 微信授权
  * @author hongcj
  * @version V1.0
  * @since 2017-08-08 19:11
@@ -24,16 +25,19 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/weixin")
 @Slf4j
 public class WeixinController {
+    public final String APPID = "wxfc26418b74dcb0a8";
+    public final String APPSECRET = "fadb448b94aa080266315a3607b689ea";
+
     @GetMapping("/auth")
     public void auth(@RequestParam("code") String code){
         log.info("进入auth方法");
         log.info("code={}",code);
+        //获取code后调用获取token接口，获取token，在获取token接口的返回集里有我们需要的openId
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + APPID + "&secret=" + APPSECRET
+            + "&code=" + code + "&grant_type=authorization_code";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response={}", response);
     }
-
-    @GetMapping("/auth2")
-    public void auth2(){
-        log.info("进入auth2方法");
-    }
-
 }
 
